40534a697870bab07ad60c25a7998a7954869bc3
Yukta Lalwani <74182344+yuktalalwani20@users.noreply.github.com>
2022-03-06 17:03:07 +0530

    Update major_project.py

--- major_project.py
+++ major_project.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
