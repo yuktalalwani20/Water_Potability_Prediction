cd216f239fea59013af2afa09cef1f14c74d01d3
Yukta Lalwani <74182344+yuktalalwani20@users.noreply.github.com>
2022-04-13 10:56:34 +0530

    Update major_project.py

--- major_project.py
+++ major_project.py
@@ -15,9 +15,9 @@ import joblib
 import base64
 
 
-data = pd.read_csv('new_data.csv')
+data = pd.read_csv('water_potability_2.csv')
 data = data.iloc[:,0:]
-original_data = pd.read_csv('new_data.csv')
+original_data = pd.read_csv('water_potability_2.csv')
 #feature_list = {'ph':0,'Turbidity':0,'pH_Type':0,'Turbidity_Type':0}
 
 classifier = joblib.load('algorithms.sav')
